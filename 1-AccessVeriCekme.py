b2da8aec7f406fe1c3f172bd5356cfdb4d0734fd
Berkay <berkayyalcin7@icloud.com>
2018-11-01 17:06:23 +0300

    Update 1-AccessVeriCekme.py
    Access Veri Kaynağı - Python Bağlantısı

--- 1-AccessVeriCekme.py
+++ 1-AccessVeriCekme.py
@@ -1,20 +1,29 @@
 
-#ODBC ÜZERİNDEN VERİ KAYNAĞINI ÇEKEBİLMEK İÇİN (pyppyodbc) kütüphanesini import ediyoruz.
+"""
+ODBC Üzerinden Oluşturduğumuz Access Veri Kaynağını Python'a Entegre Edebilmek için
+Python'da bulunan :
+-pypyodbc (Kütüphanesinden yararlanıyoruz)..
+
+"""
 import pypyodbc
 
-
-
-
+"""
+ 'connect' fonksiyonuyla düzenlediğimiz Veri Kaynağını giriyoruz.
+"""
 con=pypyodbc.connect('DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};UID=admin;UserCommitSync=Yes;Threads=3;SafeTransactions=0;PageTimeout=5;MaxScanRows=8;MaxBufferSize=2048FIL={MS Access};DriverId=25;DefaultDir=C:/Users/Dogruyer_5/Documents;DBQ=C:/Users/Dogruyer_5/Documents/Database1.accdb')
 
-
+"""
+Veritabanını cursor 'ın içine atıyoruz.
+"""
 cursor=con.cursor()
 
-#Access ' te bulunan 'test' tablosundaki verileri çekme işlemi
+"""
+Veritabanı içindeki Tablodan SQL Sorgusuyla Örnek Veri Çekme İşlemi
+
+"""
 cursor.execute('SELECT * FROM test')
 
 
-
-#verileri döngü içerisinde Gösteriyoruz.
+""" Verileri for döngüsü içerisinde Ekrana Bastırma """
 for row in cursor.fetchall():
     print(row)
